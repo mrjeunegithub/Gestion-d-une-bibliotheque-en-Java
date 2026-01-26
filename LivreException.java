9c2b1895f89b28da1efed9bc437b3fe56af3a2fb
0205NGONO <ngonoambassa2@gmail.com>
2026-01-26 02:32:54 +0100

    Add LivreException class for custom exception handling

---
+++ LivreException.java
@@ -0,0 +1,9 @@
+class LivreException extends Exception {
+    public LivreException(String message) {
+        super(message);
+    }
+    
+    public LivreException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
